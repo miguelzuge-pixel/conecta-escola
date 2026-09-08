fdb1f22a27fe5972ad2991cc15dfeacb8a1ffbf5
arthurmoraessantos-code <arthur.moraes.santos@escola.pr.gov.br>
2026-09-08 11:48:18 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,56 @@
+const loginForm = document.getElementById("loginForm");
 
+if (loginForm) {
+
+    loginForm.addEventListener("submit", function(event) {
+
+        event.preventDefault();
+
+        const email =
+            document.getElementById("email").value;
+
+        const tipo =
+            document.getElementById("tipoUsuario").value;
+
+        localStorage.setItem("usuarioEmail", email);
+
+        localStorage.setItem("tipoUsuario", tipo);
+
+        window.location.href = "index.html";
+
+    });
+
+}
+const usuario = document.getElementById("usuario");
+
+if (usuario) {
+
+    const email =
+        localStorage.getItem("usuarioEmail");
+
+    const tipo =
+        localStorage.getItem("tipoUsuario");
+
+    if (email) {
+
+        usuario.innerHTML =
+            `Você entrou como <strong>${tipo}</strong>.
+             <br>
+             E-mail: ${email}`;
+
+    } else {
+
+        window.location.href = "login.html";
+
+    }
+
+}
+function sair() {
+
+    localStorage.removeItem("usuarioEmail");
+
+    localStorage.removeItem("tipoUsuario");
+
+    window.location.href = "login.html";
+
+}
